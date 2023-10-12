693b84d3f4cd1b2a8923720174b27b88e67827ff
Osuna0102 <osuna.alejo@gmail.com>
2023-10-12 00:04:08 -0500

    ---

--- templates/script.js
+++ templates/script.js
@@ -1,3 +1,34 @@
+let startTime = Date.now();
+let wordCount = 0;
+
+// Timer
+setInterval(() => {
+    const elapsed = Math.floor((Date.now() - startTime) / 1000);
+    const minutes = Math.floor(elapsed / 60);
+    const seconds = elapsed % 60;
+    document.querySelector('#timer').textContent = `Time: ${minutes}:${seconds.toString().padStart(2, '0')}`;
+}, 1000);
+
+// Word Counter
+setInterval(() => {
+    const transcript = document.querySelector('#transcript-container');
+    const text = transcript.textContent.trim();
+    const characterCount = text.length;
+    document.querySelector('#counter').textContent = `Characters: ${characterCount}`;
+}, 1000);
+
+// Words per Minute
+setInterval(() => {
+    const transcript = document.querySelector('#transcript-container');
+    const text = transcript.textContent.trim();
+    const characterCount = text.length;
+    const elapsed = Math.floor((Date.now() - startTime) / 1000);
+    const minutes = elapsed / 60;
+    const wpm = Math.floor(characterCount / minutes);
+    document.querySelector('#wpm').textContent = `WPM: ${wpm}`;
+}, 1000);
+
+
 navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
     if (!MediaRecorder.isTypeSupported('audio/webm'))
         return alert('Browser not supported')
